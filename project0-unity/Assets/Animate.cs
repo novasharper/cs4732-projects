ebca4b66e31f566388aa682edec073d8dcb0e8c5
Patrick Long <pat@novasharper.net>
2017-03-17 17:42:30 -0400

    Added Unity Project

---
+++ project0-unity/Assets/Animate.cs
@@ -0,0 +1,30 @@
+﻿/* Module      : Animate.cs
+ * Author      : Patrick Long
+ * Email       : pllong@wpi.edu
+ * Course      : CS 4732
+ *
+ * Description : Animate a Cube by rotating it
+ *
+ * Date        : 2017/03/15
+ *
+ * History:
+ * Revision      Date          Changed By
+ * --------      ----------    ----------
+ * 01.00         2017/03/15    pllong
+ * First release.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Animate : MonoBehaviour {
+	/* Function    : void Update( void )
+	 *
+	 * Description : Runs once per tick, updates the cube's rotation
+	 */
+	void Update () {
+		// Rotate along the axis (0, sqrt(2)/2, sqrt(2)/2)
+		// Rotate 10 degrees per second
+		transform.Rotate (new Vector3 (0, 1, 1), 10 * Time.deltaTime);
+	}
+}
---
+++ project0-unity/README.txt
@@ -0,0 +1,11 @@
+Patrick Long (pllong@wpi.edu)
+CS 4732
+Project 0
+
+Hello Cube!
+
+For this project I used Unity, specifically Unity 5.4.3, so this or a later version will definitely work.
+I think earlier versions will work as well. Basically, what I did was add a Cube, then add the script
+Animate to the Cube. Animate rotates the cube 10 degrees per second.
+
+Video: https://youtu.be/PGbPAe4mO80
