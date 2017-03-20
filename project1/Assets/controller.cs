642633a312fcc34f0d7eb1880ba633ab698f40e1
Patrick Long <pat@novasharper.net>
2017-03-20 19:56:22 -0400

    Initial commit of project1

---
+++ project1/Assets/controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+
+        // create new colors array where the colors will be created.
+        Color[] colors = new Color[vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+            colors[i] = new Color(vertices[i].x, vertices[i].y, vertices[i].z);
+
+        // assign the array of colors to the Mesh.
+        mesh.colors = colors;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
